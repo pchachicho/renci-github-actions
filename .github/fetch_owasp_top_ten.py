19b21d1d2d1c05a9fe9de5538ceee21ceaa24992
Patrick Hachicho <hachichopatrick@gmail.com>
2024-08-08 16:55:16 -0400

    testing new branch

--- .github/fetch_owasp_top_ten.py
+++ .github/fetch_owasp_top_ten.py
@@ -19,3 +19,14 @@ if response.status_code == 200:
     print("PR template updated successfully.")
 else:
     print(f"Failed to fetch content. Status code: {response.status_code}")
+
+
+      # - name: Update PR template
+    #   run: |
+    #     owasp_top_ten=$(cat owasp_top_ten.txt | awk '{print "- " $0}' | paste -sd '\n' -)
+    #     echo "OWASP Top Ten:"
+    #     echo "$owasp_top_ten"
+    #     echo "Debug: escaping special characters"
+    #     printf '%s' "$owasp_top_ten" | sed 's/[&/\]/\\&/g'  
+    #     sed -i "s|\[The OWASP Top Ten list will be fetched and updated by a GitHub Action.\]|$owasp_top_ten|" .github/pull_request_template.md
+    #     echo "Updated PR Template:"
